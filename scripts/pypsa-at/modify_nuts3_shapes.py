7a806c8e6d3cae7b032c3c99db6698046fde36aa
Philip Worschischek <philip.worschischek@aggm.at>
2025-09-24 12:22:37 +0000

    build DE5 custom clustering

--- scripts/pypsa-at/modify_nuts3_shapes.py
+++ scripts/pypsa-at/modify_nuts3_shapes.py
@@ -9,7 +9,7 @@ import sys
 
 import geopandas as gpd
 
-from scripts._helpers import configure_logging, mock_snakemake
+from scripts._helpers import configure_logging
 
 logger = logging.getLogger(__name__)
 
@@ -31,6 +31,7 @@ def override_nuts(nuts_code: str | tuple, override: str, level: str = "level1") 
     Returns
     -------
     :
+        Updates the NUTS codes in the nuts_regions output file.
     """
     logger.debug(f"Overriding regions with code {nuts_code} to {override}.")
     mask = nuts3_regions.index.str.startswith(nuts_code)
@@ -57,7 +58,7 @@ def assert_expected_number_of_entries(nuts_code: str, expected: int, lvl: int = 
     """
     regions_at_level = nuts3_regions.query(f"level{lvl}.str.startswith(@nuts_code)")
     entries = regions_at_level[f"level{lvl}"].unique()
-    if not IS_TEST_RUN:
+    if snakemake.config["run"]["prefix"] != "test-sector-myopic-at10":
         assert len(entries) == expected
 
 
@@ -69,8 +70,6 @@ if __name__ == "__main__":
 
     configure_logging(snakemake)
     config = snakemake.config
-
-    IS_TEST_RUN = snakemake.config["run"]["prefix"] == "test-sector-myopic-at10"
 
     admin_levels = snakemake.params.get("admin_levels")
     nuts3_regions = gpd.read_file(snakemake.input.nuts3_shapes).set_index("index")
@@ -91,6 +90,32 @@ if __name__ == "__main__":
     if admin_levels.get("AT") == 2:
         override_nuts("AT333", "AT333", "level2")
         assert_expected_number_of_entries("AT", expected=10, lvl=2)
+    # DE: NUTS1 (19 regions) or NUTS3 as a proxy for 5 regions
+    if admin_levels.get("DE") == 3:
+        # update NUTS3 codes to contain 5 unique codes for clustering algorithm
+        # Baden-Württemberg (BW)
+        override_nuts("DE1", "DE1", level="level3")
+        # Bavaria
+        override_nuts("DE2", "DE2", level="level3")
+        # Midwest (HE, RP, SL, NW)
+        override_nuts("DE7", "DE3", level="level3")
+        override_nuts("DEB", "DE3", level="level3")
+        override_nuts("DEC", "DE3", level="level3")
+        override_nuts("DEA", "DE3", level="level3")
+        # Mideast (BB, BE, MV, SN, ST, TH)
+        override_nuts("DE3", "DE4", level="level3")
+        override_nuts("DE4", "DE4", level="level3")
+        override_nuts("DE8", "DE4", level="level3")
+        override_nuts("DED", "DE4", level="level3")
+        override_nuts("DEE", "DE4", level="level3")
+        override_nuts("DEG", "DE4", level="level3")
+        # North (SH, HH, HB, NI)
+        override_nuts("DEF", "DE5", level="level3")
+        override_nuts("DE6", "DE5", level="level3")
+        override_nuts("DE9", "DE5", level="level3")
+        override_nuts("DE5", "DE5", level="level3")
+
+        assert_expected_number_of_entries("DE", expected=5, lvl=3)
     # IT: italy is in test network but must not be clustered to reduce test complexity
     assert admin_levels.get("IT") == 1
     override_nuts("IT", "IT0")  # mainland
