928863cc0e425c46cd12da38b2f8d11f27d41fcf
Philip Worschischek <philip.worschischek@aggm.at>
2025-11-05 21:49:24 +0000

    revert scenario clustering settings

--- scripts/pypsa-at/configure_run.py
+++ scripts/pypsa-at/configure_run.py
@@ -14,9 +14,14 @@ import yaml
 
 
 @click.command(short_help="Overwrite existing values in config/config.at.yaml")
-@click.option("--scenario", "-s", type=str, required=True)
-@click.option("--randomize", "-r", type=bool, required=True)
-def configure(scenario: str, randomize: bool) -> None:
+@click.option("--clustering", type=str, required=True)
+@click.option("--resolution", type=str, required=True)
+@click.option("--solver", type=str, required=True)
+@click.option("--scenario", type=str, required=True)
+@click.option("--randomize", type=bool, required=True)
+def configure(
+    clustering: str, resolution: str, solver: str, scenario: str, randomize: bool
+) -> None:
     """
     Configure PyPSA-AT model run by updating configuration parameters.
 
@@ -25,8 +30,16 @@ def configure(scenario: str, randomize: bool) -> None:
 
     Parameters
     ----------
-    scenario
+    clustering : {'AT10DE5', 'AT35DE5', 'AT10DE19', 'AT35DE19'}
+        The name of the administrative custom clustering.
+    resolution
+        The temporal resolution in hours to set as the `sectoral_resolution`.
+    solver : {'highs', 'gurobi'}
+        The solver to use. Sets `solver_name` and `solver-options` in the configuration.
+    scenario {'AT_KN2040'}
+        The scenario name to run.
     randomize
+        The random seed for the random number generator.
 
     Returns
     -------
@@ -38,6 +51,26 @@ def configure(scenario: str, randomize: bool) -> None:
     This function is expected to run using pipelines and the dumped configuration
     yaml is not expected to be checked in to VCS.
     """
+    # validate inputs
+    accepted_solver = ("highs", "gurobi")
+    if solver not in accepted_solver:
+        raise click.BadParameter(
+            f"'{solver}' is not a valid solver. Chose from {accepted_solver}."
+        )
+
+    available_clustering = ("AT10DE5", "AT35DE5", "AT10DE19", "AT35DE19")
+    if clustering not in available_clustering:
+        raise click.BadParameter(
+            f"'{clustering}' is not valid. Chose from {available_clustering}"
+        )
+
+    # sanitize temporal resolution
+    resolution = int(resolution.rstrip("H"))
+    if resolution < 24 and solver != "gurobi":
+        raise ValueError(
+            f"Denying to run model with resolution {resolution} and solver '{solver}'."
+        )
+
     config_yaml_fp = Path("config/config.at.yaml")
     pixi_toml_fp = Path("pixi.toml")
 
@@ -57,15 +90,24 @@ def configure(scenario: str, randomize: bool) -> None:
     with pixi_toml_fp.open("rb") as fh:
         pixi = tomllib.load(fh)
 
-    # validate config
-    resolution = config["clustering"]["temporal"]["resolution_sector"]
-    resolution = int(resolution.rstrip("H"))
-    # solver configuration ruled by scenario config
-    solver_name = config["solving"]["solver"]["name"]
-    if resolution < 24 and solver_name != "gurobi":
-        raise ValueError(
-            f"Denying to run high resolution run with '{resolution}H' and solver '{solver_name}'"
-        )
+    logger.info(f"Configuring PyPSA-AT model for clustering {clustering}.")
+    config["mods"]["modify_nuts3_shapes"] = clustering
+
+    nuts_at = 2 if "AT10" in clustering else 3  # NUTS3
+    nuts_de = 1 if "DE19" in clustering else 3  # DE5
+    logger.info(
+        f"Setting administrative clustering in AT to "
+        f"NUTS level {nuts_at} (={10 if nuts_at == 2 else 35} Regions)"
+    )
+    config["clustering"]["administrative"]["AT"] = nuts_at
+    logger.info(
+        f"Setting administrative clustering in DE to "
+        f"NUTS level {nuts_de} (={19 if nuts_de == 1 else 5} Regions)"
+    )
+    config["clustering"]["administrative"]["DE"] = nuts_de
+
+    logger.info(f"Setting temporary resolution to '{resolution}H'")
+    config["clustering"]["temporal"]["resolution_sector"] = f"{resolution}H"
 
     logger.info(f"Setting scenario name to '{scenario}'")
     config["run"]["name"] = [scenario]
@@ -76,15 +118,16 @@ def configure(scenario: str, randomize: bool) -> None:
     # also overwrite the PyPSA-EUR default config version to avoid confusion
     config["version"] = version
 
-    seed = random.randint(1, 50000) if randomize else 123
-    logger.info(f"Setting seed to '{seed}'")
-    solver_options = config["solving"]["solver"]["options"]
+    if randomize:
+        seed = random.randint(1, 50000)
+        logger.info(f"Setting seed to '{seed}'")
+        solver_options = config["solving"]["solver"]["options"]
 
-    key = "random_seed" if solver_name == "highs" else "Seed"  # gurobi
-    config["solving"]["solver_options"][solver_options][key] = seed
-    # also set duplicated default setting
-    config["solving"].setdefault("options", {})
-    config["solving"]["options"]["seed"] = seed
+        key = "random_seed" if solver == "highs" else "Seed"  # gurobi
+        config["solving"]["solver_options"][solver_options][key] = seed
+        # also set duplicated default setting
+        config["solving"].setdefault("options", {})
+        config["solving"]["options"]["seed"] = seed
 
     with config_yaml_fp.open("w") as fh:
         yaml.dump(config, fh)
