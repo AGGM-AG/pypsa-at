c4a5f412a2901f9e9e10df8a6c95a53a3fb4d00e
root <nicolezechneraggm>
2026-07-06 11:31:22 +0000

    lowering threshold capacity

--- .marimo/check-biogas-plants.py
+++ .marimo/check-biogas-plants.py
@@ -13,7 +13,7 @@ def _():
 
 @app.cell
 def _(pypsa):
-    n2025_file = r"results/feat-biogas-brownfield-austria/AT_KN2040/networks/base_s_adm__none_2025.nc"
+    n2025_file = r"results/biogas-global-threshold-2MW/AT_KN2040/networks/base_s_adm__none_2025.nc"
     n2025 = pypsa.Network(n2025_file)
     return (n2025,)
 
@@ -39,6 +39,50 @@ def _(n2025):
 @app.cell
 def _(n2025):
     n2025.generators[n2025.generators.carrier == "biogas"].p_nom
+    return
+
+
+@app.cell
+def _():
+    import pandas as pd
+
+    ppl = pd.read_csv(
+        "resources/feat-biogas-brownfield-austria/AT_KN2040/powerplants_s_adm-overwrite.csv"
+    )
+    ppl
+    return (ppl,)
+
+
+@app.cell
+def _(ppl):
+    bio_e = ppl[ppl.Fueltype == "Bioenergy"]
+    bio_e_at = bio_e[bio_e.Country == "AT"]
+    return (bio_e_at,)
+
+
+@app.cell
+def _(bio_e_at):
+    bio_e_at
+    return
+
+
+@app.cell
+def _(bio_e_at):
+    bus_cap = bio_e_at.groupby("bus")["Capacity"].sum()
+    bus_cap[bus_cap > 0]  # .sum()
+    return
+
+
+@app.cell
+def _(pypsa):
+    b2025_file = r"resources/biogas-global-threshold-2MW/AT_KN2040/networks/base_s_adm__none_2025_brownfield.nc"
+    base2025 = pypsa.Network(b2025_file)
+    return (base2025,)
+
+
+@app.cell
+def _(base2025):
+    base2025.links[base2025.links["carrier"] == "biogas"]
     return
 
 
