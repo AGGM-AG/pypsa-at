34cfdba36804747539e38867f79b1609efb404de
Florian Wiedemair <florian.wiedemair@d-fine.com>
2026-09-03 08:42:10 +0000

    Add script to create nuts3 onshore regions

--- rules/pypsa-at/build_electricity.smk
+++ rules/pypsa-at/build_electricity.smk
@@ -16,3 +16,19 @@ use rule process_cost_data as process_cost_data_at with:
 
 
 ruleorder: process_cost_data_at > process_cost_data
+
+rule create_onshore_regions_nuts3:
+    input:
+        regions=resources_shared("regions_onshore_base_s_{clusters}.geojson"),
+        shapes=resources_shared("nuts3_shapes.geojson"),
+    output:
+        regions_nuts3=resources_shared("regions_onshore_base_s_{clusters}_nuts3.geojson"),
+    log:
+        logs_shared("create_onshore_regions_nuts3_{clusters}.log"),
+    benchmark:
+        benchmarks_shared("create_onshore_regions_nuts3_{clusters}")
+    threads: 1
+    message:
+        "Building NUTS3 onshore regions geojson"
+    script:
+        scripts("pypsa-at/create_onshore_regions_nuts3.py")
---
+++ scripts/pypsa-at/create_onshore_regions_nuts3.py
@@ -0,0 +1,57 @@
+import geopandas as gpd
+import logging
+
+
+import pandas as pd
+
+from scripts._helpers import configure_logging, set_scenario_config
+from snakemake.script import Snakemake
+
+logger = logging.getLogger(__name__)
+
+
+def main(snakemake: Snakemake) -> None:
+    """
+    Create a geopandas frame of onshore regions with NUTS3 level for Austria
+
+    Parameters
+    ----------
+    snakemake
+        Snakemake object providing input, parameters and output.
+
+    Returns
+    -------
+    :
+        Writes output to snakemake output paths.
+    """
+    regions_df = gpd.read_file(
+        snakemake.input.regions
+    )
+    crs = regions_df.crs
+
+    shapes_df = gpd.read_file(
+        snakemake.input.shapes
+    ).to_crs(crs)
+
+    out = pd.concat(
+        [
+            regions_df.query("~name.str.startswith('AT')"),
+            shapes_df.query("country == 'AT'")[["level3", "geometry"]].rename(
+                columns={"level3": "name"}
+            ).drop_duplicates(),
+        ]
+    )
+    out_gpd = gpd.GeoDataFrame(out, crs=crs)
+    out_gpd.to_file(snakemake.output.regions_nuts3)
+
+if __name__ == "__main__":
+    if "snakemake" not in globals():
+        from scripts._helpers import mock_snakemake
+
+        snakemake = mock_snakemake("create_onshore_regions_nuts3", clusters="adm")
+
+    configure_logging(snakemake)
+    set_scenario_config(snakemake)
+    main(snakemake)
+
+
